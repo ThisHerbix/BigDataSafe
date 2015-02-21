939d5d7d6bb132882e5018e553daef34555c20cb
alexis <fabbro@et.esiea.fr>
2015-02-21 18:05:41 +0100

    Premier commit Récupération de tweets, insertion dans une Base MongoDB

---
+++ Big Data/Data/__init__.py
@@ -0,0 +1,25 @@
+import time
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import os
+from listener import listener
+import pymongo
+from pymongo import MongoClient
+
+
+ckey = 'LLPdEoKpDy2eS0SH3Q51kTJLm'
+consumer_secret = '3tC736rpxgLAMr7DTCapNnCUPOHhWJVp9dcdfvDSzFVmiCD95R'
+access_token_key = '2917878929-jBWv0AZkEarot5z1XYLVManLBgGYAmZ8wZsMLZx'
+access_token_secret = 'EOJYh4SF5n2Q8zaK059Bfb8nKV7AjILXaqjFHIxRzcqYb'
+ 
+ 
+start_time = time.time() #grabs the system time
+keyword_list = ['football', 'soccer'] #track list
+
+auth = OAuthHandler(ckey, consumer_secret) #OAuth object
+auth.set_access_token(access_token_key, access_token_secret)
+
+
+twitterStream = Stream(auth, listener(start_time, time_limit=20)) #initialize Stream object with a time out limit
+twitterStream.filter(track=keyword_list, languages=['en'])  #call the filter method to run the Stream Object
---
+++ Big Data/Data/listener.py
@@ -0,0 +1,44 @@
+import time
+from tweepy.streaming import StreamListener
+from pymongo import MongoClient
+import json
+
+
+class listener(StreamListener):
+
+    def __init__(self, start_time, time_limit=60):
+
+        self.time = start_time
+        self.limit = time_limit
+    
+    def on_data(self, data):
+        while (time.time() - self.time) < self.limit:
+            try:
+                client = MongoClient('localhost', 27017)
+                db = client['twitter_db']
+                collection = db['twitter_collection']
+                tweet = json.loads(data)
+                collection.insert(tweet)
+                
+                #saveFile = open('raw_tweets.json', 'a')
+                #saveFile.write(data)
+                #saveFile.write('\n')
+                #saveFile.close()
+                print data
+               
+
+                return True
+
+
+            except BaseException, e:
+                print 'failed ondata,', str(e)
+                time.sleep(5)
+                pass
+
+        exit()
+        
+    def on_error(self, status):
+
+        print status
+        
+  
