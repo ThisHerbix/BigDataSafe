c019e56b36e1ad36d9de39a9513e058e737948ab
alexis <fabbro@et.esiea.fr>
2015-02-27 19:02:56 +0100

    Essaie dico

--- Big Data/Traitement/__init__.py
+++ Big Data/Traitement/__init__.py
@@ -2,6 +2,9 @@ from pymongo import MongoClient
 import pymongo
 from pymongo.collection import Collection
 import re
+from reportlab.graphics.barcode.eanbc import words
+from OneWordInfo import OneWordInfo
+import pickle
 
 
 #Cette fonction retourne le nombre de fois qu'un mot du sac de mot a ete rencontre.
@@ -10,7 +13,7 @@ def TwtWhichContainOneWord(collection, searchedWord):
     return nombreOccurence
 
 
-#Cette fonction retourne la somme du nombre de tweet contenant les mots du sac de mot donnes en parametre auront ete rencontre.
+#Cette fonction retourne la somme du nombre de tweet contenant les mots du sac de mot donne en parametre auront ete rencontre.
 #ATTENTION Ce nombre peut donc depasser le nombre de tweet contenu dans la base !
 def TwtWhichContainOneWordOfABagOfWord(collection, *bagOfSearchedWord):
     nombreOccurence = 0
@@ -32,6 +35,40 @@ def twtWhichContainABagOfWord(collection, *bagofword):
                                                ,{'text' : {'$regex' : '.*' + wordList[4] +'.*'}}]}).count()
     return nombreOccurence
 
+def retrieveTwt(collection):
+    for obj in collection.find({'text':{'$exists' : True}}):
+        if obj:
+            text = obj['text']
+            print text
+def LoadDictionnary(collection):
+    dictionnary = dict()
+    dictionnary = {}
+    for obj in collection.find({'text':{'$exists' : True}}):
+        if obj:
+            text = obj['text']
+            words = re.split(r'[,; _\/\\]*',text)
+            for word in range(len(words)):
+                if dictionnary.has_key(words[word]):
+                    value = dictionnary[words[word]]
+                    dictionnary[words[word]] = value+1
+                else: 
+                    dictionnary[words[word]] = 1   
+    return dictionnary
+
+def OneTwtDictionnary(twt, dictionnary):
+    dictionnary = dict()
+    dictionnary = {}
+    words = twt.split()
+    for word in range(len(words)):
+        if dictionnary.has_key(words[word]):
+            value = dictionnary[words[word]]
+            dictionnary[words[word]] = value+1
+        else: 
+            dictionnary[words[word]] = 1               
+    return dictionnary
+ 
+       
+
 client = MongoClient('localhost', 27017)
 db = client['twitter_db']
 collection = db['twitter_collection']
@@ -39,12 +76,23 @@ collection = db['twitter_collection']
 bagofword = ['football', 't', 'r','e', 'z']
 
 
-a = TwtWhichContainOneWord(collection,'ok')
-print "Nb tweets contenant ok = "+str(a) 
-a = TwtWhichContainOneWord(collection,'football')
-print "Nb tweets contenant football = "+str(a) 
-a = TwtWhichContainOneWordOfABagOfWord(collection, *bagofword)
-print "Nb tweets contenant l'un des mots du sac de mot = "+str(a) 
-a = twtWhichContainABagOfWord(collection, *bagofword)
-print 'Nb MAIN tweet contenant tous les mots du sac = '+str(a)
+#a = TwtWhichContainOneWord(collection,'ok')
+#print "Nb tweets contenant ok = "+str(a) 
+#a = TwtWhichContainOneWord(collection,'football')
+#print "Nb tweets contenant football = "+str(a) 
+#a = TwtWhichContainOneWordOfABagOfWord(collection, *bagofword)
+#print "Nb tweets contenant l'un des mots du sac de mot = "+str(a) 
+#a = twtWhichContainABagOfWord(collection, *bagofword)
+#print 'Nb tweet contenant tous les mots du sac = '+str(a)
+#retrieveTwt(collection)
 
+a = dict()
+#a = OneTwtDictionnary('ok ok ok prout find test find test dans ta geule geule geule', a)
+a = LoadDictionnary(collection)
+#output = open('/home/alexis/Bureau/Text.file','wb')
+#pickle.dump(a, output)
+#output.close()
+
+v = list(a.values())
+k = list(a.keys())
+print k[v.index(max(v))]
